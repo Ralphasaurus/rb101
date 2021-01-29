7a17c19d0cd4bc12197471bc1397b13ffead27e1
Ralph Hartman <ralph_e@hotmail.com>
2021-01-29 22:54:09 +0000

    refactored lizard spock game

--- lesson2/lizard_spock.rb
+++ lesson2/lizard_spock.rb
@@ -1,45 +1,76 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = %w(r p s l k)
 
-WINNING_MOVES = { rock: ["scissors", "lizard"], paper: ["rock", "spock"],
-                  scissors: ["paper", "lizard"], lizard: ["spock", "paper"],
-                  spock: ["rock", "scissors"] }
-@player_score = 0
-@computer_score = 0
+WINNING_MOVES = { r: ["s", "l"], p: ["r", "k"],
+                  s: ["p", "l"], l: ["k", "p"],
+                  k: ["r", "s"] }
+scores = { player: 0, computer: 0 }
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def display_score
-  prompt("YOUR SCORE: #{@player_score}")
-  prompt("COMPUTER SCORE: #{@computer_score}")
-end
-
-def score_board(points)
-  points ? @player_score += 1 : @computer_score += 1
-end
-
-def display_results(players_choice, computer_choice)
-  if WINNING_MOVES[players_choice.to_sym].include?(computer_choice)
-    prompt("You won!")
-    score_board(true)
-  elsif WINNING_MOVES[computer_choice.to_sym].include?(players_choice)
-    prompt("Computer won!")
-    score_board(false)
-  else prompt("It's a tie!")
+def validate_player_choice(choice)
+  if VALID_CHOICES.include?(choice)
+    true
+  else
+    prompt("That's not a valid choice. Try again.")
+    false
   end
 end
 
-def win?
-  if @player_score == 5
+def winner?(players_choice, computer_choice, scores)
+  if WINNING_MOVES[players_choice.to_sym].include?(computer_choice)
+    display_who_won(1, scores)
+  elsif WINNING_MOVES[computer_choice.to_sym].include?(players_choice)
+    display_who_won(2, scores)
+  else display_who_won(3, scores)
+  end
+end
+
+def display_who_won(result, scores)
+  case result
+  when 1
+    prompt("You won this round!")
+    score_board(true, scores)
+  when 2
+    prompt("Computer won this round!")
+    score_board(false, scores)
+  when 3
+    prompt("It's a tie!")
+  end
+end
+
+def score_board(points, scores)
+  points ? scores[:player] += 1 : scores[:computer] += 1
+end
+
+def display_score(scores)
+  prompt("YOUR SCORE: #{scores[:player]}")
+  prompt("COMPUTER SCORE: #{scores[:computer]}")
+end
+
+def reset_points(scores)
+  scores[:player] = 0
+  scores[:computer] = 0
+end
+
+def match_over?(scores)
+  (scores[:player]) == 5 || (scores[:computer]) == 5
+end
+
+def evaluate_final_winner(scores)
+  scores[:player] == 5
+end
+
+def display_final_winner(final_result)
+  if final_result == true
     prompt("******Congratulations, you are the grand champion!******")
   else
     prompt("******The computer is the grand champion!******")
   end
-  prompt(display_score())
 end
 
-prompt("Welcome to LIZARD SPROCK!")
+prompt("Welcome to LIZARD SPOCK!")
 prompt("-------------------------")
 prompt("Here are the rules of the game:")
 prompt("SCISSORS > PAPER > ROCK > LIZARD > SPOCK > SCISSORS")
@@ -54,26 +85,24 @@ loop do
   loop do
     loop do
       prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-      choice = gets.chomp
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice. Try again.")
-      end
+      choice = gets.chomp.downcase
+      break if validate_player_choice(choice)
     end
 
     computer_choice = VALID_CHOICES.sample()
-
+    system("clear")
     prompt("You chose: #{choice}. Computer chose: #{computer_choice}")
 
-    display_results(choice, computer_choice)
-    display_score()
-    if @player_score == 5 || @computer_score == 5
-      break win?()
-    end
+    winner?(choice, computer_choice, scores)
+    display_score(scores)
+    break if match_over?(scores)
   end
-  prompt("Do you want to play again?")
+
+  display_final_winner(evaluate_final_winner(scores))
+
+  prompt("Do you want to play again? (Y to continue, any other key to quit)")
   continue = gets.chomp
+  reset_points(scores)
   break unless continue.downcase.start_with?("y")
 end
 
