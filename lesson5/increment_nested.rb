024e27d4676dd65d12ea894efc236d76a7be6435
Ralph Hartman <ralph_e@hotmail.com>
2021-03-02 04:44:54 +0000

    finished probelm 10

--- lesson5/increment_nested.rb
+++ lesson5/increment_nested.rb
@@ -4,9 +4,10 @@ array = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
 # array, use the map method to return a new array identical in structure 
 # to the original but where the value of each integer is incremented by 1.
 
-new_array = array.map do |hsh|
-  hsh.each_value do |value|
-    p value += 1 # this isn't working because each is returning the original hash.. fix!
+array.map do |hsh|
+  new_hash = {}
+  hsh.each do |key, value|
+    new_hash[key] = value += 1
   end
+  new_hash
 end
-p new_array
