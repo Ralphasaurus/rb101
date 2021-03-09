8fc2bb48015c1269a0e72b7c980e4bb4a316ec11
Ralph Hartman <ralph_e@hotmail.com>
2021-03-09 04:21:13 +0000

    did problem 5 and 6 of easy6 tonight

---
+++ small_problems/easy6/problem_6.rb
@@ -0,0 +1,11 @@
+# Write a method that takes two Arrays as arguments, and returns an Array that
+# contains all of the values from the argument Arrays. There should be no duplication
+# of values in the returned Array, even if there are duplicates in the original Arrays.
+
+def merge(array1, array2)
+  array1.concat(array2).uniq
+end
+
+# the set union works well... array1 | array2
+
+p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
