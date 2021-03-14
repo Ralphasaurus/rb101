ceecc84941d43294a10160b72e370d420c58dcd8
Ralph Hartman <ralph_e@hotmail.com>
2021-03-14 18:20:57 +0000

    did problem 9

--- small_problems/easy7/problem_9.rb
+++ small_problems/easy7/problem_9.rb
@@ -5,13 +5,28 @@
 
 # You may assume that neither argument is an empty Array.
 
-# // input: two arrays containing a list of numbers that can be formed
+# // input: two arrays containing a list of numbers
 # // output: new array that contains the product of every pair, sorted in increasing value
 
-def multiply_all_pairs(array1, array2)
-  p array1.zip(array2) { |pair| p pair } 
+# // for each element of array1
+# // multiply it by each element in array 2
+# // store these values in an array
+# // sort array
+
+# def multiply_all_pairs(array1, array2)
+#   result_array = []
+#   array1.each do |num1|
+#     array2.each { |num2| result_array << (num2 * num1) }
+#   end
+#   result_array.sort
+# end
+
+#Compact Solution!
+
+def multiply_all_pairs(arr1,arr2)
+  arr1.product(arr2).map {|arr| arr.reduce(:*)}.sort
 end
 
 # Test Case
 
-multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
