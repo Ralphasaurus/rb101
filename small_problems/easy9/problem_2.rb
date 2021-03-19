79243d3b641cdc5bf5553df81109a65824b72788
Ralph Hartman <ralph_e@hotmail.com>
2021-03-19 18:28:30 +0000

    finished problem2

--- small_problems/easy9/problem_2.rb
+++ small_problems/easy9/problem_2.rb
@@ -12,17 +12,38 @@
 # // need to first evaluate to see if it is a double number (helper method?)
 # // depending upon this result either return the number or double it and return it.
 
+def double?(int)
+  array = int.to_s.chars
+  first = []
+  last = []
+  if array.size.odd?
+    false
+  else
+    loop do
+      break if array.empty?
+      first << array.shift
+      last << array.pop
+    end
+    first == last.reverse ? true : false
+  end
+    
+end
 
-twice(37) == 74
-twice(44) == 44
-twice(334433) == 668866
-twice(444) == 888
-twice(107) == 214
-twice(103103) == 103103
-twice(3333) == 3333
-twice(7676) == 7676
-twice(123_456_789_123_456_789) == 123_456_789_123_456_789
-twice(5) == 10
+def twice(int)
+  double?(int) ? int : int * 2
+end
+
+
+p twice(37) == 74
+p twice(44) == 44
+p twice(334433) == 668866
+p twice(444) == 888
+p twice(107) == 214
+p twice(103103) == 103103
+p twice(3333) == 3333
+p twice(7676) == 7676
+p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+p twice(5) == 10
 
 # Note: underscores are used for clarity above. Ruby lets you use underscores
 # when writing long numbers; however, it does not print the underscores when
