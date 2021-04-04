5ce7605d0fe00e76ba5397e4fc61747b0775cf2a
Ralph Hartman <ralph_e@hotmail.com>
2021-04-04 03:40:35 +0000

    working on the medium small problems

---
+++ small_problems/medium1/problem_5.rb
@@ -0,0 +1,10 @@
+# Write a method that displays a 4-pointed diamond in an n x n grid, where n
+# is an odd integer that is supplied as an argument to the method. You may assume
+# that the argument will always be an odd integer.
+
+
+def diamond(n)
+  
+end
+
+diamond(9)
